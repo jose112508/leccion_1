ef9a7bb13f25721d590b20b8fc73a67df53dc22f
jose112508 <andrewvivargmail.com>
2022-07-21 18:17:49 -0500

    SuboTarea

---
+++ java/app/leccion1.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app;
+
+import javax.swing.JOptionPane;
+
+
+
+public class leccion1 {
+
+    public static void main(String[] args) {
+        //entrada
+     
+        
+        String nombre="" ,  carrera= "";
+        int codigo= 0,numeroMateria= 0 ;
+        nombre =JOptionPane.showInputDialog(null,"ingrese su nombre");
+        carrera =JOptionPane.showInputDialog(null,"ingrese su carrera");
+        codigo =Integer.parseInt.(JOptionPane.showInputDialog(null,"ingrese su codigo"));
+        numeroMateria =Integer.parseInt.(JOptionPane.showInputDialog(null,"ingrese el numero de materias "));
+        
+        //proceso
+        
+   
+        
+        }
+        
+        
+        
+        
+        
+        // salida 
+  
+
+}
